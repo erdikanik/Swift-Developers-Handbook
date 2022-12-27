dab8f932f2f13873013c5c2b17d0d0e5d1c46fb1
ka-ni-ke <erdikanik@gmail.com>
2022-12-27 14:05:50 +0300

    Create the project

---
+++ SwiftDevelopersHandbook/SwiftDevelopersHandbook/SwiftDevelopersHandbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDevelopersHandbookApp.swift
+//  SwiftDevelopersHandbook
+//
+//  Created by Erdi Kanık on 27.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDevelopersHandbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
